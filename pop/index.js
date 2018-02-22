25dc596bc52e23812250aafb78af5737377b2f0c
Yurihaia <17830663+Yurihaia@users.noreply.github.com>
2018-02-22 15:45:59 -0500

    oops

--- pop/index.js
+++ pop/index.js
@@ -1,15 +1,6 @@
 var audio = document.getElementById("audio");
 var fish = document.getElementById("img");
 
-var currentTimeout = null;
-
-function runChainRec(func, time, i) {
-    currentTimeout = window.setTimeout(() => {
-        func[i]();
-        runChain(func, time, i + 1);
-    }, time[i]);
-}
-
 function toggle() {
     if(audio.paused) {
         audio.play();
