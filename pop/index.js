82f7a1887b1afa973b8e5edef313201ea294f119
Yurihaia <17830663+Yurihaia@users.noreply.github.com>
2018-02-22 14:46:49 -0500

    MOAR quality

---
+++ pop/index.js
@@ -0,0 +1,12 @@
+var audio = document.getElementById("audio");
+var fish = document.getElementById("img");
+
+function toggle() {
+    if(audio.paused) {
+        audio.play();
+        fish.setAttribute("src", "pufferfish_heart.png");
+    } else {
+        audio.pause();
+        fish.setAttribute("src", "pufferfish.png");
+    }
+}
