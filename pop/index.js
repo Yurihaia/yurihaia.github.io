0cf2f1032ae9d0c0d8ef09ba14e40cd8549c2fcc
Yurihaia <17830663+Yurihaia@users.noreply.github.com>
2019-03-25 12:45:51 -0400

    Add pufferfish tongue option
    (add ?tongue at the end of the URL)

--- pop/index.js
+++ pop/index.js
@@ -1,10 +1,16 @@
 var audio = document.getElementById("audio");
 var fish = document.getElementById("img");
 
+let tongue = new URLSearchParams(location.search).has("tongue");
+
+if(tongue) {
+    fish.classList.add("tonguefish");
+}
+
 function toggle() {
     if(audio.paused) {
         audio.play();
-        fish.setAttribute("src", "pufferfish_heart.png");
+        fish.setAttribute("src", tongue? "pufferfish_tongue.png": "pufferfish_heart.png");
     } else {
         audio.pause();
         fish.setAttribute("src", "pufferfish.png");
