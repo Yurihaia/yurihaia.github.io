72e85ade35b6577817817a73994418b56c4d0579
Yurihaia <17830663+Yurihaia@users.noreply.github.com>
2019-11-29 13:05:02 -0500

    start carrot

---
+++ carrot/index.js
@@ -0,0 +1,14 @@
+var audio = document.getElementById("audio");
+var fish = document.getElementById("img");
+
+function toggle() {
+    if(audio.paused) {
+        audio.play();
+        fish.setAttribute("src", "pufferfish_carrot.png");
+    } else {
+        audio.pause();
+        fish.setAttribute("src", "pufferfish.png");
+    }
+}
+
+document.onclick = toggle;
